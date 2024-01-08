1317abefa800a6f4cfa595af1e5483c7797f2ab5
[BEER]CHANADEE✅ <145905321+chanadeecnd@users.noreply.github.com>
2024-01-08 15:21:56 +0700

    Update draw_circle.py

--- draw_circle.py
+++ draw_circle.py
@@ -5,7 +5,7 @@ img = cv2.imread('./image/cat.jpg')
 #resize
 size_img = cv2.resize(img, (600, 480))
 
-#draw circle => cv2.circle(image, center, redius, color, weight)
+#draw circle => cv2.circle(image, center, radius, color, weight)
 cv2.circle(size_img, (300, 240), 100, (0,0,255), 5)
 
 cv2.imshow('Output', size_img)
